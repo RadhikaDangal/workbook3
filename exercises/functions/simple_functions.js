9e0f93bfb453c3bc950ac470786e05f1b271a7b8
Radhika Dangal <rdangal@pgh.yearup.org>
2023-05-01 11:40:08 -0400

    initial commit

---
+++ exercises/functions/simple_functions.js
@@ -0,0 +1,13 @@
+function favoriteThings() {
+    console.log("My favorite things are: ");
+    console.log("My favorite movie is 3 Idiots.");
+    console.log("My favorite Band is 1974 AD.");
+}
+
+favoriteThings();
+
+function whyImHere(){
+    console.log(`I really like students and their attitudes and feel like I'm making a difference in their lives.`);
+}
+
+whyImHere();
