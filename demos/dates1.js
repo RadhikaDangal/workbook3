11e3b617a85a8ded735a8c9b4f289c19834a9ca5
Radhika Dangal <rdangal@pgh.yearup.org>
2023-05-03 15:52:12 -0400

    updates

--- demos/dates1.js
+++ demos/dates1.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 // let date ={
 //     month: 1,
 //     year:1987,
@@ -10,22 +10,27 @@
 //         return `month, year, date`
 // };
 
-let date =new Date();
-console.log(date);
-console.log(date.toString());
-console.log(date.toUTCString());
-console.log(date.toDateString());
+// let date =new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toUTCString());
+// console.log(date.toDateString());
 
-console.log(date.toLocaleDateString());
-console.log(date.toLocaleTimeString());
-console.log(date.toLocaleString());
-//console.log(Date.getDay());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toLocaleString());
+// //console.log(Date.getDay());
 
+// let anticipatedDate = new Date(2023, 5, 22,7, 15, 30,10);
+// console.log(anticipatedDate.toString());
 
-let anticipatedDate = new Date(2023, 5, 22,7, 15, 30,10);
-console.log(anticipatedDate.toString());
-
-let anotherDate = new Date("2023-06-22T07:10Z");
-console.log(anotherDate.toString());
+// let anotherDate = new Date("2023-06-22T07:10Z");
+// console.log(anotherDate.toString());
 
 let d5 = new Date("2004-06-25");
+
+let d = new Date();
+d.setMonth(1);
+d.setDate(9);
+d.setFullYear(1963);
+console.log(d.toDateString());
