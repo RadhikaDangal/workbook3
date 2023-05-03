5ac62aab517a1b904bc6f4a13bb8b1c8cbd578d5
Radhika Dangal <rdangal@pgh.yearup.org>
2023-05-03 10:32:07 -0400

    updates

---
+++ demos/dates1.js
@@ -0,0 +1,31 @@
+"use strict"
+// let date ={
+//     month: 1,
+//     year:1987,
+//     day:21,
+//     toString: function(){
+//         return `month, year, date`
+//     },
+//     toUTCString: function(){
+//         return `month, year, date`
+// };
+
+let date =new Date();
+console.log(date);
+console.log(date.toString());
+console.log(date.toUTCString());
+console.log(date.toDateString());
+
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+console.log(date.toLocaleString());
+//console.log(Date.getDay());
+
+
+let anticipatedDate = new Date(2023, 5, 22,7, 15, 30,10);
+console.log(anticipatedDate.toString());
+
+let anotherDate = new Date("2023-06-22T07:10Z");
+console.log(anotherDate.toString());
+
+let d5 = new Date("2004-06-25");
